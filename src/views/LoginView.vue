28e1abd4385c09013523c3df14010efd86b814e0
bipuladhikari <bipuladk60@gmail.com>
2024-05-20 14:03:19 -0400

    requirements met

---
+++ src/views/LoginView.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-container class="fill-height"
+    ><v-row align="center" justify="center">
+      <v-col cols="12" sm="8" md="4">
+        <v-card class="elevation-6">
+          <v-toolbar color="purple">
+            <v-toolbar-title class="title"
+              >Log in to Your Account</v-toolbar-title
+            >
+          </v-toolbar>
+          <v-card-text>
+            <v-form @submit.prevent="login">
+              <v-text-field
+                v-model="username"
+                label="Username"
+                prepend-icon="mdi-account"
+              />
+              <v-text-field
+                v-model="password"
+                type="password"
+                label="Password"
+                prepend-icon="mdi-lock"
+              />
+              <v-btn type="submit" color="purple" class="btn">Login</v-btn>
+              <v-alert v-if="error" type="error">{{ errorMessage }}</v-alert>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row></v-container
+  >
+</template>
+<script>
+export default {
+  name: "LoginView",
+  data: () => ({
+    username: "",
+    password: "",
+    error: false,
+    errorMessage: "",
+  }),
+  methods: {
+    login() {
+      if (this.username === "example" && this.password === "password") {
+        this.$router.push({ name: "WelcomeView" });
+      } else {
+        this.error = true;
+        this.errorMessage = "Incorrect username/password";
+      }
+    },
+  },
+};
+</script>
+<style>
+.title {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+  word-spacing: -5px;
+  font-family: "Courier New", Courier, monospace;
+}
+.btn {
+  width: 100%;
+  font-weight: bold;
+  font-family: "Courier New", Courier, monospace;
+}
+.v-label {
+  font-family: "Courier New", Courier, monospace;
+}
+</style>
---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <h1>Welcome, user</h1>
+    <v-btn @click="$router.push({ name: 'LoginView' })"
+      ><v-icon left>mdi-logout</v-icon>Logout</v-btn
+    >
+  </v-container>
+</template>
+<script>
+export default {
+  name: "WelcomeView",
+};
+</script>
+<style>
+h1 {
+  font-family: "Courier New", Courier, monospace;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  word-spacing: -5px;
+}
+.v-btn {
+  font-family: "Courier New", Courier, monospace;
+  font-weight: bold;
+  color: white;
+  background-color: red;
+}
+</style>
